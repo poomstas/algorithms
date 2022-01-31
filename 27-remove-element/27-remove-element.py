43ef313d912a9913034388fca97ca6e164b1f456
Brian Lee <b.lee@utexas.edu>
2022-01-31 11:06:15 +0900

    Time: 56 ms (20.53%), Space: 13.8 MB (98.82%) - LeetHub

--- 27-remove-element/27-remove-element.py
+++ 27-remove-element/27-remove-element.py
@@ -2,15 +2,33 @@ class Solution:
     def removeElement(self, nums: List[int], val: int) -> int:
         a, b = 0, len(nums)-1
         while a <= b:
-            if nums[a]==val and nums[b]!=val:
-                nums[a], nums[b] = nums[b], nums[a]
+            val_a, val_b = nums[a], nums[b]
+            if val_a==val and val_b!=val:
+                nums[a], nums[b] = val_b, val_a 
                 a += 1
                 b -= 1
-            elif nums[a]!=val and nums[b]==val:
+            elif val_a!=val and val_b==val:
                 a += 1
                 b -= 1
-            elif nums[a]!=val and nums[b]!=val:
+            elif val_a!=val and val_b!=val:
                 a += 1
             else:
                 b -= 1
         return a
+    
+# class Solution:
+#     def removeElement(self, nums: List[int], val: int) -> int:
+#         a, b = 0, len(nums)-1
+#         while a <= b:
+#             if nums[a]==val and nums[b]!=val:
+#                 nums[a], nums[b] = nums[b], nums[a]
+#                 a += 1
+#                 b -= 1
+#             elif nums[a]!=val and nums[b]==val:
+#                 a += 1
+#                 b -= 1
+#             elif nums[a]!=val and nums[b]!=val:
+#                 a += 1
+#             else:
+#                 b -= 1
+#         return a
