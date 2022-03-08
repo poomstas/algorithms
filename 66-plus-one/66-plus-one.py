9f16ad145fbe2d57e63a70aabc4cfb72f0ef60ab
Brian Lee <b.lee@utexas.edu>
2022-03-08 15:52:05 +0900

    Time: 36 ms (80.07%), Space: 14 MB (34.68%) - LeetHub

--- 66-plus-one/66-plus-one.py
+++ 66-plus-one/66-plus-one.py
@@ -1,10 +1,9 @@
 class Solution:
     def plusOne(self, digits: List[int]) -> List[int]:
         if digits[-1] == 9:
-            number = 0
+            number = 1 # Start with 1 added. Same as starting with 0 and adding 1 later.
             for i, value in enumerate(reversed(digits)):
                 number += value * 10**(i)
-            number += 1
             return list(str(number))
         else:
             digits[-1] += 1
