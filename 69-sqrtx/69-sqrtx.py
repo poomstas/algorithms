6cd31f28323dd7040e61471a1fbfa3d4fb196e1e
Brian Lee <b.lee@utexas.edu>
2022-06-02 05:40:50 +0900

    Time: 57 ms (49.47%), Space: 13.8 MB (56.45%) - LeetHub

--- 69-sqrtx/69-sqrtx.py
+++ 69-sqrtx/69-sqrtx.py
@@ -1,9 +1,20 @@
+# Binary Search Solution
+# O(Nlog(N)) time | O(1) space
+
 class Solution:
     def mySqrt(self, x: int) -> int:
-        int_sqrt = 0
-        while True:
-            squared = int_sqrt * int_sqrt
-            if squared <= x:
-                int_sqrt += 1
-            else:
-                return int_sqrt-1
+        if x==0 or x==1:
+            return x
+
+        L, R = 0, x
+        M = (L + R)//2
+        
+        while L <= R:
+            if M*M < x:
+                L = M+1
+            elif M*M > x:
+                R = M-1
+            elif M*M == x:
+                break
+            M = (L + R)//2      # The placement of this matters. Work through the iters by hand.
+        return M        
