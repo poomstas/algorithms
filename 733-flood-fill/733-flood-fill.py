8cd256a9e8c965f3f5eda86e32cdaf76da443ee5
Brian Lee <b.lee@utexas.edu>
2022-06-03 19:39:14 +0900

    Time: 82 ms (79.61%), Space: 14.1 MB (65.84%) - LeetHub

---
+++ 733-flood-fill/733-flood-fill.py
@@ -0,0 +1,21 @@
+class Solution:
+    def floodFill(self, image: List[List[int]], sr: int, sc: int, newColor: int) -> List[List[int]]:
+        R, C = len(image), len(image[0])
+        queue = [[sr, sc]]
+        startingCol = image[sr][sc]
+        while len(queue):
+            print(queue)
+            r, c = queue.pop()
+            image[r][c] = newColor
+            if self.validCoord(r+1, c, R, C) and image[r+1][c]==startingCol and image[r+1][c]!=newColor: # up
+                queue.append([r+1, c])
+            if self.validCoord(r-1, c, R, C) and image[r-1][c]==startingCol and image[r-1][c]!=newColor: # down
+                queue.append([r-1, c])
+            if self.validCoord(r, c-1, R, C) and image[r][c-1]==startingCol and image[r][c-1]!=newColor: # left
+                queue.append([r, c-1])
+            if self.validCoord(r, c+1, R, C) and image[r][c+1]==startingCol and image[r][c+1]!=newColor: # right
+                queue.append([r, c+1])
+        return image
+    
+    def validCoord(self, r, c, R, C):
+        return r >= 0 and r < R and c >= 0 and c < C
