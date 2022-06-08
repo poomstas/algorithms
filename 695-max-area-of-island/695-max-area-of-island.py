e76f110dbcc540bf91bf07ddeacbdb977f7ee9ac
Brian Lee <b.lee@utexas.edu>
2022-06-08 13:04:43 +0900

    Time: 204 ms (43.64%), Space: 16.5 MB (57.37%) - LeetHub

--- 695-max-area-of-island/695-max-area-of-island.py
+++ 695-max-area-of-island/695-max-area-of-island.py
@@ -1,38 +1,33 @@
 class Solution:
     def maxAreaOfIsland(self, grid: List[List[int]]) -> int:
-        self.R, self.C = len(grid), len(grid[0])
-        self.visited = [[False for _ in range(self.C)] for _ in range(self.R)]
-        max_area = 0
-        for r in range(self.R):
-            for c in range(self.C):
-                if grid[r][c]==1:
-                    area = self.exploreIsland(grid, r, c)
-                    if area > max_area:
-                        max_area = area
-        return max_area
-    
-    def exploreIsland(self, grid, r_init, c_init):
-        island_area = 0
-        queue = [[r_init, c_init]]
-        while queue:
-            r, c = queue.pop(0)
-            if self.visited[r][c] == True:
-                continue
-            self.visited[r][c] = True
-            island_area += 1
-            queue.extend(self.getValidIslandNeighbors(grid, r, c))
-        return island_area
-    
-    def getValidIslandNeighbors(self, grid, r, c):
-        neighbors = []
-        if grid[r][c]==0:
-            return neighbors
-        if r > 0 and grid[r-1][c]==1 and not self.visited[r-1][c]:
-            neighbors.append([r-1, c])
-        if r < self.R-1 and grid[r+1][c]==1 and not self.visited[r+1][c]:
-            neighbors.append([r+1, c])
-        if c > 0 and grid[r][c-1]==1 and not self.visited[r][c-1]:
-            neighbors.append([r, c-1])
-        if c < self.C-1 and grid[r][c+1]==1 and not self.visited[r][c+1]:
-            neighbors.append([r, c+1])
-        return neighbors
+        max_island = 0
+        R, C = len(grid), len(grid[0])
+        for r in range(R):
+            for c in range(C):
+                if grid[r][c] != 1: # 0 or 2
+                    continue
+                island_size = self.exploreIsland(grid, r, c, R, C)
+                if island_size > max_island:
+                    max_island = island_size
+        return max_island
+        
+    def exploreIsland(self, grid, r, c, R, C):
+        if not self.validCoord(r, c, R, C) or grid[r][c] != 1: 
+            return 0
+        
+        grid[r][c] = 2  # Mark visited
+        count = 1       # Count current spot
+
+        if self.validCoord(r-1, c, R, C) and grid[r-1][c] == 1:
+            count += self.exploreIsland(grid, r-1, c, R, C)
+        if self.validCoord(r+1, c, R, C) and grid[r+1][c] == 1:
+            count += self.exploreIsland(grid, r+1, c, R, C)
+        if self.validCoord(r, c-1, R, C) and grid[r][c-1] == 1:
+            count += self.exploreIsland(grid, r, c-1, R, C)
+        if self.validCoord(r, c+1, R, C) and grid[r][c+1] == 1:
+            count += self.exploreIsland(grid, r, c+1, R, C)
+        return count
+        
+        
+    def validCoord(self, r, c, R, C):
+        return r>=0 and c>=0 and r<=R-1 and c<=C-1
