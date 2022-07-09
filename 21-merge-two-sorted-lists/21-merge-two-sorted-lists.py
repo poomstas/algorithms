8721193a346badb5cfd620cf1cb0ed69486c1a57
Brian Lee <b.lee@utexas.edu>
2022-07-10 08:33:26 +0900

    Time: 45 ms (78.90%), Space: 13.9 MB (32.31%) - LeetHub

--- 21-merge-two-sorted-lists/21-merge-two-sorted-lists.py
+++ 21-merge-two-sorted-lists/21-merge-two-sorted-lists.py
@@ -3,28 +3,22 @@
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
-
 class Solution:
     def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
-        if list1 is None and list2 is None:
-            return None
+        out = ListNode()
+        curr = out
         
-        output = ListNode()
-        current_node_1, current_node_2, current_node_out = list1, list2, output
-        
-        while current_node_1 is not None or current_node_2 is not None:
-            val_1 = current_node_1.val if current_node_1 is not None else float('inf')
-            val_2 = current_node_2.val if current_node_2 is not None else float('inf')
+        while list1 is not None or list2 is not None:
+            list1val = list1.val if list1 else float('inf')
+            list2val = list2.val if list2 else float('inf')
             
-            if val_1 <= val_2:
-                current_node_out.val = val_1
-                current_node_1 = current_node_1.next
+            if list1val <= list2val:
+                curr.next = ListNode(list1val)
+                list1 = list1.next if list1 else None
             else:
-                current_node_out.val = val_2
-                current_node_2 = current_node_2.next
+                curr.next = ListNode(list2val)
+                list2 = list2.next if list2 else None
+            curr = curr.next
                 
-            if current_node_1 is not None or current_node_2 is not None:
-                current_node_out.next = ListNode()
-                current_node_out = current_node_out.next
-                
-        return output
+        return out.next
+        
