c7f59ea6733ffc8a9674a4e94a31264d40d3bd83
Brian Lee <b.lee@utexas.edu>
2022-07-18 22:59:28 +0900

    Time: 578 ms (27.22%), Space: 16.2 MB (97.39%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.py
+++ 200-number-of-islands/200-number-of-islands.py
@@ -1,38 +1,38 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
-        self.R, self.C = len(grid), len(grid[0])
+        R, C = len(grid), len(grid[0])
+        self.visited = [[False for _ in range(C)] for _ in range(R)] # Boolean map
         island_count = 0
-        self.visited = [[False for _ in range(self.C)] for _ in range(self.R)] # Boolean map
-        for r in range(self.R):
-            for c in range(self.C):
+        
+        for r in range(R):
+            for c in range(C):
                 if not self.visited[r][c] and grid[r][c]=='1':
-                    self.exploreIsland(grid, r, c)
+                    self.traverseIsland(grid, r, c, R, C)
                     island_count += 1
                 else:
                     self.visited[r][c] = True
         return island_count
         
-    def exploreIsland(self, grid: List[List[str]], r_init: int, c_init: int) -> None:
+    def traverseIsland(self, grid, r_init, c_init, R, C) -> None:
         queue = [[r_init, c_init]]
         while queue:
             r, c = queue.pop()
             self.visited[r][c] = True
-            queue.extend(self.getValidIslandNeighbors(grid, r, c))
+            queue.extend(self.getValidIslandNeighbors(grid, r, c, R, C))
             
-    def getValidIslandNeighbors(self, grid, r, c):
+    def getValidIslandNeighbors(self, grid, r, c, R, C):
         neighbors = []
         if grid[r][c]=='0':
             return neighbors
         
         if r >= 1 and not self.visited[r-1][c] and grid[r-1][c]=='1':
             neighbors.append([r-1, c])
-        if r < self.R-1 and not self.visited[r+1][c] and grid[r+1][c]=='1':
+        if r < R-1 and not self.visited[r+1][c] and grid[r+1][c]=='1':
             neighbors.append([r+1, c])
         if c >= 1 and not self.visited[r][c-1] and grid[r][c-1]=='1':
             neighbors.append([r, c-1])
-        if c < self.C-1 and not self.visited[r][c+1] and grid[r][c+1]=='1':
+        if c < C-1 and not self.visited[r][c+1] and grid[r][c+1]=='1':
             neighbors.append([r, c+1])
             
         return neighbors
             
-        
