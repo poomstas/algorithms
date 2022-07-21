b642b53e5c6c78239239a067cd8af650f8a93410
Brian Lee <b.lee@utexas.edu>
2022-07-21 10:53:58 +0900

    solved algoexpert binary tree diameter (longest path calculator)

--- binary_tree_diameter.py
+++ binary_tree_diameter.py
@@ -7,27 +7,35 @@ import unittest
 # Get a practice on explaining how the algorithm works. That's more important than the coding part.
 
 # O(N) time | O(H) space avg, O(N) max
-def binaryTreeDiameter(tree):
-	return getTreeInfo(tree).diameter
-
-def getTreeInfo(tree):
-	if tree is None:
-		return TreeInfo(0, 0)
-	
-	leftTreeInfo = getTreeInfo(tree.left)
-	rightTreeInfo = getTreeInfo(tree.right)
-	
-	longestPathThroughRoot = leftTreeInfo.height + rightTreeInfo.height
-	maxDiameterSoFar = max(leftTreeInfo.diameter, rightTreeInfo.diameter)
-	currentDiameter = max(longestPathThroughRoot, maxDiameterSoFar)
-	currentHeight = 1 + max(leftTreeInfo.height, rightTreeInfo.height)
-	
-	return TreeInfo(currentDiameter, currentHeight)
-	
+class BinaryTree:
+    def __init__(self, value, left=None, right=None):
+        self.value = value
+        self.left = left
+        self.right = right
 class TreeInfo:
-	def __init__(self, diameter, height):
-		self.diameter = diameter
-		self.height = height
+    def __init__(self, diameter, height):
+        self.diameter = diameter
+        self.height = height
+
+
+def binaryTreeDiameter(tree):
+    return getTreeInfo(tree).diameter
+
+def getTreeInfo(node):
+    if node is None:
+        return TreeInfo(0, 0)
+
+    L = getTreeInfo(node.left)    # Left Subtree Info
+    R = getTreeInfo(node.right)   # Right Subtree Info
+
+    # Determine Current Node's Tree Diameter
+    max_path_thru_node = L.height + R.height
+    node_diameter = max(L.diameter, R.diameter, max_path_thru_node)
+
+    # Determine Current Node's Tree Height
+    node_height = max(L.height, R.height) + 1
+
+    return TreeInfo(node_diameter, node_height)
 	
 
 ######################################################################3
